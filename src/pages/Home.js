8199ab01c03ac8d3fd2019c42af2d2a3b2aca684
rohit-raje-786 <rp589006@gmail.com>
2022-11-21 13:31:38 +0530

    prod env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function Home() {
-  return (
-    <div className="App">
-      <h1> Home</h1>
-      <a href="/about">About</a>
-      <br />
-      <a href="/contact">Contact</a>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1> Home(Production)</h1>
+			<a href="/about">About</a>
+			<br />
+			<a href="/contact">Contact</a>
+		</div>
+	);
 }
 
 export default Home;
